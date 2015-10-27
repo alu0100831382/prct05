10be071d6943f8b9442900cbbad414cbf694f1a0
alu0100831382 <alu0100831382@ull.edu.es>
2015-10-27 12:11:22 +0000

    Comprobando la reduccion de la fraccion

--- lib/fraccionario_test_v1.rb
+++ lib/fraccionario_test_v1.rb
@@ -1,8 +1,8 @@
-require './fraccionario'
+require './fraccionarios'
 require 'test/unit'
 
 class TestPoint < Test::Unit::TestCase
   def test_simple
-    assert_equal("(1/2)", Fraccionario.new(1,2).to_s)
+    assert_equal("(1/2)", Fraccionario.new(2,4).to_s)
   end
 end
--- lib/fraccionarios.rb
+++ lib/fraccionarios.rb
@@ -4,11 +4,30 @@ class Fraccionario
     
     attr_reader :n, :d
     
+    #Inicializas las variables
     def initialize(n,d)
-        @n, @d = n, d
+        @n, @d = reducir(n, d)
     end
     
+    #Metodo que devuelve la representacion
     def to_s
         "(#{@n}/#{@d})"
     end
+    
+    #Metodo para calcular el minimo comun multiplo
+    def lcm(a, b)
+		a, b = a.abs, b.abs
+		(a / gcd(a, b)) * b
+	end
+	
+    #Metodo para reducir la fraccion
+    def reducir(n, d)
+        aux = gcd(n, d)
+        n = n / aux
+        d = d / aux
+        return n, d
+    end
+    
+    
+    
 end
