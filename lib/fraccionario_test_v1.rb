1c77f55a53c69d6ce16bafbc62f66f9e210bd5b5
alu0100831382 <alu0100831382@ull.edu.es>
2015-10-28 11:08:01 +0000

    Añadiendo la division y comprobando test

--- lib/fraccionario_test_v1.rb
+++ lib/fraccionario_test_v1.rb
@@ -3,17 +3,30 @@ require 'test/unit'
 
 class TestPoint < Test::Unit::TestCase
   
+  #Test simple
   def test_simple
     assert_equal("(1/2)", Fraccionario.new(2,4).to_s)
   end
   
+  #Test que suma los numeros racionales
   def test_suma
     assert_equal([22,15], Fraccionario.new(2,3).suma(4,5)) 
     assert_equal([1,1], Fraccionario.new(1,2).suma(2,4)) 
   end
   
+  #Test que comprueba la resta de los numeros racionales
+  def test_resta
+    assert_equal([1,3], Racional.new(2,3).resta(1,3)) 
+    assert_equal([7,6], Racional.new(5,3).resta(1,2))
+  end
+  
+  #Test que comprueba la multiplicacion
   def test_multiplicacion
     assert_equal([1,1], Fraccionario.new(2/1).multi(1,2))
   end
   
+  #Test que comprueba la division
+  def test_division
+    assert_equal([4,2], Fraccionario.new(1,2).dividir(1,4))
+  end
 end
--- lib/fraccionarios.rb
+++ lib/fraccionarios.rb
@@ -28,7 +28,7 @@ class Fraccionario
         return n, d
     end
     
-    #Metodo que suma los numeros
+    #Metodo que suma los dos numeros racionales
     def suma(n, d)
         if(@d ==d)
            @n += n
@@ -41,11 +41,37 @@ class Fraccionario
         return @n,@d
     end
     
-    #Metodo que multiplica los dos numeros
+    #Metodo que resta los dos numeros racionales
+    def resta(n, d)
+        if(@d == d)
+            @n -= n
+            @n,@d = reducir(@n, @d)
+        else
+            new_d = mcm(@d, d)
+            new_n = ((new_d / @d) *@n ) - ((new_d / d) *n )    
+            @n, @d = reducir(new_n, new_d)
+        end
+        return @n,@d
+    end
+    
+    #Metodo que multiplica los dos numeros racionales
     def multiplicar(n, d)
-        n,d = reducir(n, d)
+        n, d = reducir(n, d)
+        
         @n *= n
         @d *= d
+        
+        reducir(@n, @d)
+        return @n, @d
+    end
+    
+    #Metodo que divide los dos numeros racionales
+    def division(n, d)
+        n, d = reducir(n, d)
+        
+        @n *= d
+        @d *= n
+        
         reducir(@n, @d)
         return @n, @d
     end
